b684536902d885852427f99601009dfad8b92768
AoEiuV020 <aoeiuv020@gmail.com>
2026-01-03 17:37:45 +0800

    添加flutter空模块

---
+++ projects/flutter_im/packages/im_sdk_api/example/im_sdk_api_example.dart
@@ -0,0 +1,6 @@
+import 'package:im_sdk_api/im_sdk_api.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ projects/flutter_im/packages/im_sdk_api/test/im_sdk_api_test.dart
@@ -0,0 +1,16 @@
+import 'package:im_sdk_api/im_sdk_api.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
